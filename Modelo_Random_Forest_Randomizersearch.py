11f04bfc1ce909c9ba17a5e1398885b1ebc688ff
DavidZuletaM <dazuleta@bancolombia.com.co>
2025-04-05 20:37:40 -0500

    Update Modelo_Random_Forest_Randomizersearch.py
    Actualización del script. Se incluyen los códigos que realizan la creación del dataset que se utilizará para entrenar el modelo.

--- Modelo_Random_Forest_Randomizersearch.py
+++ Modelo_Random_Forest_Randomizersearch.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import json
+from sklearn.model_selection import train_test_split
 
 # Creación de los Dataframes
 df_transactions = pd.read_csv("data/transactions_data.csv",sep=",")
@@ -14,20 +14,41 @@ df_users_data = pd.read_csv("data/users_data.csv", sep=",")
 df_users_data.to_parquet("data/users_data.parquet", index=False)
 df_users_data = pd.read_parquet("data/users_data.parquet")
 
-with open("data/mcc_codes.json", "r", encoding="utf-8") as f:    
-    data = json.load(f)
-df_mcc = pd.DataFrame.from_dict(data, orient='index')
-df_mcc.reset_index(inplace=True)
-df_mcc.columns = ["mcc", "descripcion"]
+# Creación del Dataset para entrenar y testear el modelo de Machine Learning
+df_union = (
+    df_transactions
+    .merge(
+        df_cards_data[["id","credit_limit"]], 
+        right_on="id",
+        left_on="card_id",
+        how = 'left'
+    )
+    .merge(
+        df_users_data[["id","per_capita_income","yearly_income","credit_score","current_age"]],
+        right_on="id",
+        left_on="client_id",
+        how = 'left'
+    )
+)
 
-with open("data/train_fraud_labels.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-df_train_fraud_labels = pd.DataFrame.from_dict(data, orient='index')
-df_train_fraud_labels = df_train_fraud_labels.T
-df_train_fraud_labels.reset_index(inplace=True)
-df_train_fraud_labels.columns = ["id_transaction", "target"]
+df_union["amount"] = (df_union["amount"].str.replace("$","", regex=False)).astype(float)
+df_union["credit_limit"] = (df_union["credit_limit"].str.replace("$","", regex=False)).astype(int)
+df_union["per_capita_income"] = (df_union["per_capita_income"].str.replace("$","", regex=False)).astype(int)
+df_union["yearly_income"] = (df_union["yearly_income"].str.replace("$","", regex=False)).astype(int)
 
-df_transactions["mcc"] = df_transactions["mcc"].astype(str)
-df_transactions["amount"] = (df_transactions["amount"].str.replace("$","", regex=False)).astype(float)
+df_resultado = df_union.groupby("mcc").agg({
+        "id":'count',"amount":'sum',"card_id":'nunique',
+        "zip":'nunique',"client_id":'nunique',"merchant_id":'nunique',
+        "credit_limit":'mean',"per_capita_income":'mean',
+        "yearly_income":'mean',"credit_score":'mean',"current_age":'mean'
+    }).reset_index()
 
-df2 = df_transactions.merge(df_mcc, on= 'mcc')
+#Definición de variable respuesta
+y = df_resultado['amount']
+X = df_resultado.copy()
+X.drop('amount', axis=1, inplace=True)
+
+#Preparación de los conjuntos de datos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#Pipeline
